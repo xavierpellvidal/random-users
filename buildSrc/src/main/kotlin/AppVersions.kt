2d52a9110ef488697ccaa8883c1c247b178ffd04
Pell <xavierpellvidal@gmail.com>
2025-04-21 18:07:19 +0200

    Bumped app version

--- buildSrc/src/main/kotlin/AppVersions.kt
+++ buildSrc/src/main/kotlin/AppVersions.kt
@@ -5,7 +5,7 @@ object AppVersions {
     const val COMPILE_SDK = 35
     const val MIN_SDK = 26
     const val APP_VERSION_CODE = 2
-    const val APP_VERSION_NAME = "1.1.0"
+    const val APP_VERSION_NAME = "1.1.1"
     const val JVM_TARGET = "17"
     val javaVersion = JavaVersion.VERSION_17
 }
