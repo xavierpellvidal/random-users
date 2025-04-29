8647596dc317db96385eca7a34a97ffaa2d679d1
Pell <xavierpellvidal@gmail.com>
2025-04-29 09:46:52 +0200

    Routes to interfaces

--- presentation/users/src/main/kotlin/com/random/users/users/navigation/UsersRoute.kt
+++ presentation/users/src/main/kotlin/com/random/users/users/navigation/UsersRoute.kt
@@ -3,12 +3,12 @@ package com.random.users.users.navigation
 import com.random.users.users.model.UserUiModel
 import kotlinx.serialization.Serializable
 
-internal sealed class UsersRoute {
+internal sealed interface UsersRoute {
     @Serializable
-    data object Home : UsersRoute()
+    data object Home : UsersRoute
 
     @Serializable
     data class UserDetail(
         val user: UserUiModel,
-    ) : UsersRoute()
+    ) : UsersRoute
 }
